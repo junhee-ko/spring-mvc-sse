5b49e5df690238cabf6a7cec5e5a0827d4185290
junhee-ko <junheee.ko@gmail.com>
2022-01-13 12:39:07 +0900

    first commit

---
+++ src/main/kotlin/jko/springmvcsse/SpringMvcServerSentEvetnApplication.kt
@@ -0,0 +1,11 @@
+package jko.springmvcsse
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringMvcServerSentEvetnApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringMvcServerSentEvetnApplication>(*args)
+}
---
+++ src/test/kotlin/jko/springmvcsse/SpringMvcServerSentEvetnApplicationTests.kt
@@ -0,0 +1,13 @@
+package jko.springmvcsse
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringMvcServerSentEvetnApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
